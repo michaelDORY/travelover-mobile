00ec30b99fb16e9ae9e9698070393874403e914e
anastasiiavelma <anastasiia.velma@nure.ua>
2022-06-16 13:22:53 +0300

    questions and nextButton

--- lib/screens/quizQuestion_screen.dart
+++ lib/screens/quizQuestion_screen.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:travelover_mobile/models/nav_buttons_data.dart';
+import 'package:travelover_mobile/widgets/questions_widget.dart';
 import 'package:travelover_mobile/widgets/quizQuestion.dart';
 import 'package:unicons/unicons.dart';
+import 'package:travelover_mobile/widgets/next_Questions.dart';
 
-class quizQuestionScreen extends StatelessWidget {
+class quizQuestionScreen extends StatefulWidget {
   const quizQuestionScreen({Key? key}) : super(key: key);
+
+  @override
+  State<quizQuestionScreen> createState() => _quizQuestionScreenState();
+}
+
+class _quizQuestionScreenState extends State<quizQuestionScreen> {
+  List<QuizQuestion> questions = [
+    QuizQuestion(
+        textQuestion: "Как называется столица Украины",
+        Answer1: "Харьков",
+        Answer2: "Киев",
+        Answer3: "Лондон",
+        Answer4: "Париж"),
+    QuizQuestion(
+        textQuestion: "Как называется столица Британии",
+        Answer1: "Харьков",
+        Answer2: "Киев",
+        Answer3: "Лондон",
+        Answer4: "Париж")
+  ];
+
+  int index = 0;
+  void nextQuestion() {
+    setState(() {
+      index++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,28 +48,27 @@ class quizQuestionScreen extends StatelessWidget {
             horizontal: 10.0,
             vertical: 15.0,
           ),
-          child: Column(children: const [
-            QuizQuestion(
-              textQuestion: 'Как назівается столица Франции?',
-              Answer1: 'Лондон',
-              Answer2: 'Киев',
-              Answer3: 'Париж',
-              Answer4: 'Прага',
-            ),
+          child: Column(children: [
+            questionWidget(
+                question: questions[index].textQuestion,
+                totalQuestions: questions.length,
+                indexAction: index)
+            // QuizQuestion()
+            //   textQuestion: 'Как назівается столица Франции?',
+            //   Answer1: 'Лондон',
+            //   Answer2: 'Киев',
+            //   Answer3: 'Париж',
+            //   Answer4: 'Прага',
+            // ),
           ])),
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.symmetric(
           horizontal: 20.0,
           vertical: 60.0,
         ),
-        child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              minimumSize: const Size.fromHeight(50),
-            ),
-            onPressed: () {
-              Navigator.pushNamed(context, '/quizEnd');
-            },
-            child: const Text("Следующий")),
+        child: NextQuestion(
+          nextQuestion: nextQuestion,
+        ),
       ),
     );
   }
---
+++ lib/widgets/next_Questions.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:travelover_mobile/models/nav_buttons_data.dart';
+import 'package:travelover_mobile/widgets/quizQuestion.dart';
+import 'package:unicons/unicons.dart';
+
+class NextQuestion extends StatelessWidget {
+  const NextQuestion({
+    Key? key,
+    required this.nextQuestion,
+  }) : super(key: key);
+
+  final VoidCallback nextQuestion;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: nextQuestion,
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            minimumSize: const Size.fromHeight(50),
+          ),
+          onPressed: () {
+            // Navigator.pushNamed(context, '/quizEnd');
+          },
+          child: const Text("Следующий")),
+    );
+  }
+}
