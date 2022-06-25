f80ba2d800389be9a11d204b6dc0e6ea07f121d2
anastasiiavelma <anastasiia.velma@nure.ua>
2022-06-25 12:43:56 +0300

    added change photo without saving

--- lib/screens/my_profile_screen.dart
+++ lib/screens/my_profile_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:loader_overlay/loader_overlay.dart';
 import 'package:provider/provider.dart';
@@ -7,9 +9,19 @@ import 'package:travelover_mobile/services/auth.dart';
 import 'package:travelover_mobile/utils/toast.dart';
 import 'package:unicons/unicons.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:image_picker/image_picker.dart';
 
-class MyProfile extends StatelessWidget {
+class MyProfile extends StatefulWidget {
   const MyProfile({Key? key}) : super(key: key);
+
+  @override
+  State<MyProfile> createState() => _MyProfileState();
+}
+
+class _MyProfileState extends State<MyProfile> {
+  bool circular = false;
+  final ImagePicker _picker = ImagePicker();
+  PickedFile? _imageFile;
 
   void _menuOpen(context) {
     Navigator.of(context).push(
@@ -42,7 +54,7 @@ class MyProfile extends StatelessWidget {
         ],
         title: Text(
           AppLocalizations.of(context).myProfile,
-          style: TextStyle(
+          style: const TextStyle(
             color: Colors.yellowAccent,
             fontSize: 20.0,
           ),
@@ -52,11 +64,12 @@ class MyProfile extends StatelessWidget {
           alignment: Alignment.center,
           padding: const EdgeInsets.all(30),
           child: Column(
-            children: [
-              const CircleAvatar(
-                radius: 80,
-                backgroundImage: AssetImage('assets/images/mario.jpg'),
-              ),
+            children: <Widget>[
+              imageProfile(),
+              // const CircleAvatar(
+              //   radius: 80,
+              //   backgroundImage: AssetImage('assets/images/mario.jpg'),
+              // ),
               Form(
                 child: Column(
                   children: [
@@ -92,6 +105,82 @@ class MyProfile extends StatelessWidget {
               )
             ],
           )),
+    );
+  }
+
+  Widget imageProfile() {
+    return Center(
+      child: Stack(children: <Widget>[
+        CircleAvatar(
+          radius: 80.0,
+          // ignore: unnecessary_null_comparison
+          backgroundImage: _imageFile == null
+              ? null // added this line
+              : FileImage(File(_imageFile!.path)),
+        ),
+        Positioned(
+          bottom: 20.0,
+          right: 20.0,
+          child: InkWell(
+            onTap: () {
+              showModalBottomSheet(
+                context: context,
+                builder: ((builder) => bottomSheet()),
+              );
+            },
+            child: const Icon(
+              Icons.camera_alt,
+              color: Colors.yellowAccent,
+              size: 28.0,
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+
+  void takePhoto(ImageSource source) async {
+    // ignore: deprecated_member_use
+    final pickedFile = await _picker.getImage(
+      source: source,
+    );
+    setState(() {
+      _imageFile = pickedFile!;
+    });
+  }
+
+  Widget bottomSheet() {
+    return Container(
+      height: 100.0,
+      width: MediaQuery.of(context).size.width,
+      margin: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 20,
+      ),
+      child: Column(
+        children: <Widget>[
+          const Text(
+            "Choose profile photo",
+            style: TextStyle(
+              fontSize: 20.0,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            // ignore: deprecated_member_use
+            // ignore: deprecated_member_use
+            FlatButton.icon(
+              icon: const Icon(Icons.image),
+              onPressed: () {
+                takePhoto(ImageSource.gallery);
+              },
+              label: const Text("Gallery"),
+            ),
+          ])
+        ],
+      ),
     );
   }
 }
