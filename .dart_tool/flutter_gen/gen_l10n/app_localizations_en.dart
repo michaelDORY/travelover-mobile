562544f0c83c73e0a294ab0bebbac1728a0caa17
michaelDORY <mish.makwww@gmail.com>
2022-06-28 18:08:51 +0300

    .dart-tool

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_en.dart
@@ -0,0 +1,252 @@
+
+
+
+import 'app_localizations.dart';
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get title => 'Travelover';
+
+  @override
+  String get login => 'Login';
+
+  @override
+  String get signUp => 'Sign Up';
+
+  @override
+  String get name => 'Name';
+
+  @override
+  String get authorize => 'Authorize';
+
+  @override
+  String get loginViaGoogle => 'Login via Google';
+
+  @override
+  String get or => 'or';
+
+  @override
+  String get email => 'Email';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get nothingFound => 'Nothing Found';
+
+  @override
+  String get discover => 'Discover';
+
+  @override
+  String get takeQuizes => 'Take Quizes';
+
+  @override
+  String get myProfile => 'My Profile';
+
+  @override
+  String get updateUsername => 'New name';
+
+  @override
+  String get checkKnowladge => 'Check your knowledge';
+
+  @override
+  String get needAnIdea => 'Need an idea where to go?';
+
+  @override
+  String get fav => 'Favourites';
+
+  @override
+  String get lookPlace => 'Look at your places';
+
+  @override
+  String get premium => 'Premium';
+
+  @override
+  String get needFunction => 'Need more functions?';
+
+  @override
+  String get add => 'Add';
+
+  @override
+  String get address => 'Address';
+
+  @override
+  String get totalScore => 'Question: ';
+
+  @override
+  String get logOut => 'LogOut';
+
+  @override
+  String get language => 'Language';
+
+  @override
+  String get needHelp => 'Need help?';
+
+  @override
+  String get support => 'Support';
+
+  @override
+  String get currentEmail => 'Current mail';
+
+  @override
+  String get confirm => 'Confirm';
+
+  @override
+  String get favPlaces => 'Favourite places';
+
+  @override
+  String get dontHaveFav => 'You don\'t have favourites';
+
+  @override
+  String get rate => 'Rate the place';
+
+  @override
+  String get comm => 'Comments';
+
+  @override
+  String get addComm => 'Add comment';
+
+  @override
+  String get addYourComm => 'Add your comment';
+
+  @override
+  String get toPay => '\$60/mounth';
+
+  @override
+  String get buy => 'Buy';
+
+  @override
+  String get mounthPlan => 'Mounth plan';
+
+  @override
+  String get premiumActivation => 'Premium activation';
+
+  @override
+  String get areYouShure => 'Are you sure you want to activate the premium for \$60 per month?';
+
+  @override
+  String get activate => 'Activate';
+
+  @override
+  String get cancel => 'Cancel';
+
+  @override
+  String get successActivation => 'You have successfully activated premium';
+
+  @override
+  String get ok => 'Okay';
+
+  @override
+  String get premiumSub1 => 'Our app has many benefits when you purchase a subscription. One of them is access to comments, where you can leave your impressions of the place, as well as look at reviews from others about the same place.';
+
+  @override
+  String get premiumSub2 => 'Possibility to add the place you like to \'Favorites\', as well as delete it.';
+
+  @override
+  String get premiumSub3 => 'The ability to edit your profile will improve your profile.';
+
+  @override
+  String get premiumSub4 => 'Unlimited access to premium quizzes, as well as the ability to share the result of the quiz on social networks.';
+
+  @override
+  String get quiz => 'Quiz';
+
+  @override
+  String get quizes => 'Quizes';
+
+  @override
+  String get start => 'Start';
+
+  @override
+  String get dontBeUpset => 'Don\'t be upset! Try again';
+
+  @override
+  String get rightWay => 'You are on the right way!';
+
+  @override
+  String get goodGob => 'Good job!';
+
+  @override
+  String get weelDone => 'Well done! Perfect job!';
+
+  @override
+  String get res => 'Result';
+
+  @override
+  String get scored => 'You have scored ';
+
+  @override
+  String get share => 'SHARE YOUR RESULTS';
+
+  @override
+  String get shapeLink1 => 'Look what I got in this test!I scored';
+
+  @override
+  String get shapeLink2 => 'out of 100!I completed this quiz on the incredible Travelover app! Download this app to test your knowledge!';
+
+  @override
+  String get shareButton => 'Share';
+
+  @override
+  String get home => 'Home';
+
+  @override
+  String get next => 'Next';
+
+  @override
+  String get contactUs => 'Contact us';
+
+  @override
+  String get supportText => 'Our unique TraveLover app invites users to take advantage of our support! Write to us on social networks or by mail what worries you or you notice an error. Our staff will be happy to answer you during the working day. All your suggestions will be heard. Stay with us! ';
+
+  @override
+  String get chooseTheLan => 'Choose the right language';
+
+  @override
+  String get chooseLan => 'Choose the language';
+
+  @override
+  String get hintVasya => 'Vasiliy..';
+
+  @override
+  String get changeYourName => 'Change your name';
+
+  @override
+  String get sucessChanged => 'Successfully changed!';
+
+  @override
+  String get waitApprove => 'Wait till approving';
+
+  @override
+  String get passwordIsReq => 'Password is required';
+
+  @override
+  String get passwordMustBe => 'Password must be at least 8 digits long';
+
+  @override
+  String get emailIsReq => 'Email is required';
+
+  @override
+  String get enterCorrectEmail => 'Enter correct email';
+
+  @override
+  String get nosuchUser => 'No such user';
+
+  @override
+  String get smthWentWrong => 'Something went wrong';
+
+  @override
+  String get enterCorrectFields => 'Enter correct fields';
+
+  @override
+  String get nameIsReq => 'Name is required';
+
+  @override
+  String get searching => 'Search...';
+
+  @override
+  String get searchAndFind => 'Search and find out...';
+}
